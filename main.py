8799783a87c69faddf5ecbed0c5c073997d165c4
Hongxiang Fan <hf17@ccgpu3.doc.ic.ac.uk>
2020-08-24 15:54:23 +0100

    update gitignore

--- main.py
+++ main.py
@@ -74,9 +74,9 @@ elif args.dataset == "mnist":
                                  (0.1307,), (0.3081,))
                              ])
 
-    trainset =  torchvision.datasets.MNIST('/data/', train=True, download=True,
+    trainset =  torchvision.datasets.MNIST('/mnist/', train=True, download=True,
                                 transform=data_transform)
-    testset = torchvision.datasets.MNIST('/data/', train=False, download=True,
+    testset = torchvision.datasets.MNIST('/mnist/', train=False, download=True,
                                 transform=data_transform)
     trainloader = torch.utils.data.DataLoader(
         trainset, batch_size=128, shuffle=True, num_workers=2)
